d41b713d9fd63e2170b43a780abbd9ff7cf94fd2
RitikaJain <ritika.2705@gmail.com>
2014-11-02 00:58:32 +0530

    updated
    updated

--- 20102014/NipponItSolutions/js/NipponItSolutions.js
+++ 20102014/NipponItSolutions/js/NipponItSolutions.js
@@ -1,3 +1,10 @@
-function changeImage(id, img){
-	document.getElementById(id).src=img;
+function resetContactUs(){
+	alert("hello");
+	document.getElementById("name").value='';
+	document.getElementById("company_name").value='';
+	document.getElementById("email").value='';
+	document.getElementById("telephone").value='';
+	document.getElementById("comments").value='';
+	document.getElementById("captcha_img").src = 'captcha_old.php?' + Math.random();
+	document.getElementById("captcha").value = '';				
 }
