ef572ad57de1d2634f82c3b6d211a360ace4e28d
RitikaJain <ritika.2705@gmail.com>
2014-11-17 00:19:02 +0530

    home page + dbexpertise
    home page
    db expertise
    db development
    db designinig

--- 20102014/NipponItSolutions/js/NipponItSolutions.js
+++ 20102014/NipponItSolutions/js/NipponItSolutions.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+	$("#contactUs").validate({
+		debug: false,
+		rules: {
+			name: "required",
+			telephone: "required",
+			email: {
+				required: true,
+				email: true
+			},
+			comments: "required",
+			captcha: "required"
+		},
+		errorPlacement: function(){
+			return false;
+		},
+		submitHandler: function(form) {
+			$.post('js/php/process.php', $("#contactUs").serialize(), function(data) {
+				$('#results').html(data);
+			});
+			document.getElementById('captcha_img').src = 'js/php/captcha.php?' + Math.random();
+			document.getElementById('captcha').value = '';
+		}
+	});		
+}
+);	
+
 function clearFormPlements(ele) {	 
 		$('#results').html("");
 		clearFormElements(ele);
