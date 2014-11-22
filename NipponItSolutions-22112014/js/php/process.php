6280686966654ab81a0e0afa3c84a4c24bc3ee98
RitikaJain <ritika.2705@gmail.com>
2014-11-22 19:11:54 +0530

    Version Tagged 22112014
    Version Tagged 22112014

---
+++ NipponItSolutions-22112014/js/php/process.php
@@ -0,0 +1,75 @@
+<?php
+	/*
+	Here's where you want PHP to process the form data and do something with it, for example inserting the data into a database or sending the information to an email address and so on
+	*/
+
+	session_start(); 
+	if($_POST['captcha'] != $_SESSION['digit']) 
+	{
+		print "Sorry, the code entered was incorrect!";
+		session_destroy();
+		die();
+	}
+    $email_to = "information@nipponitsolutions.com";
+    $email_subject = "[NTits website]";
+               
+    $first_name = $_POST['name']; // required
+    $company_name = $_POST['company_name']; 
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
+     
+    $error_message = "";
+
+    $email_message = "Form details below.\n\n";
+	
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+	$email_subject .= " Enquiry from ".clean_string($first_name);
+	if(strlen($company_name) > 0)
+	{
+		$email_subject .= " - ".clean_string($company_name);
+	}
+     
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+    //$email_message .= "Last Name: ".clean_string($last_name)."\n";
+	$email_message .= "Company Name: ".clean_string($company_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "How can we help you: ".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers); 
+
+
+$email_message_reply ="Thank you for your interest in Nippon Technocraft IT Solutions.\n Our team will get back with you very soon.\n";
+$email_message_reply .="\n";
+$email_message_reply .="\n";
+$email_message_reply .="From,\n";
+$email_message_reply .="Nippon Technocraft IT Solutions\n\n";
+$email_message_reply .="Address :\n";
+$email_message_reply .="5, Vishal Apartment, S. V. P. Road, Kandivali West.\n";
+$email_message_reply .="Mumbai - 400 067.\n";
+$email_message_reply .="Maharashtra.\n";
+$email_message_reply .="India.\n";
+$email_message_reply .="\n";
+$email_message_reply .="Direct Contact : +91 (0) 9821 884629 \n";
+$email_message_reply .="Fax +91 (022) 28085940 \n";
+
+// create email headers
+$email_subject_reply = "Nippon Technocraft IT Solutions - Enquiry";
+
+$headers_reply = 'From: '.$email_to."\r\n".
+'Reply-To: '.$email_to."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_from, $email_subject_reply, $email_message_reply, $headers_reply); 
+	echo "<script> resetAfterSuccess(); </script>";
+	print "Thank you for your interest in Nippon Technocraft IT Solutions.<br/> Our team will get back with you very soon.</b><br>";
+?>
