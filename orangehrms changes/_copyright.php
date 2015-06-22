e4faae66b64b0b4ae622f62841ec931264ec3b5a
vinaymistry-NT <vinaymistry@gmail.com>
2015-06-22 14:31:32 +0530

    adding publication and orange hrms changes.
    adding publication and orange hrms changes.

---
+++ orangehrms changes/_copyright.php
@@ -0,0 +1,8 @@
+<?php 
+$version = '3.2.1';
+$prodName = 'Nippon IT Solutions';
+$copyrightYear = date('Y');
+
+?>
+<?php echo $prodName . ' ' . $version;?><br/>
+&copy; 2005 - <?php echo $copyrightYear;?> <a href="http://nipponitsolutions.com" target="_blank">Nippon IT Solutions</a>. All rights reserved.
